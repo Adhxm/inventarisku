b6bf47f2ba6fbac49c7725df52a29fc5c50d02b6
Adhxm <125205326+Adhxm@users.noreply.github.com>
2023-09-11 22:34:53 +0700

    upgrade ke video ke 9
    disini saya sudah selesai hingga part 8, sekarang otw 9

---
+++ page/detail_pengembalian.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail Pengembalian</title>
+</head>
+<body>
+    <div class="col-lg-6">
+        <div class="panel panel-primary">
+            <div class="panel-heading">Konfirmasi Pengembalian Inventaris</div>
+            <div class="panel-body">
+                <form action="">
+                    <div class="form-group">
+                        <label for="" class="">Kode Peminjaman</label>
+                        <input type="text" name="" id="" class="form-control" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="">Tanggal Peminjaman</label>
+                        <input type="date" name="" id="" class="form-control" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="">Nama Peminjam</label>
+                        <input type="text" name="" id="" class="form-control" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="">Nama Barang</label>
+                        <input type="text" name="" id="" class="form-control" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="">Jumlah</label>
+                        <input type="number" name="" id="" class="form-control" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="">Tanggal Pengembalian</label>
+                        <input type="date" name="" id="" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-primary btn-md" type="submit">Simpan</button>
+                        <a href="?p=pengembalian" class="btn btn-md btn-default">Kembali</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
