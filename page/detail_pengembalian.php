96fcbd667dab0009b6bb883d6f8fe7aeb2c805f4
Adhxm <125205326+Adhxm@users.noreply.github.com>
2023-10-09 22:59:17 +0700

    part 11 sudah selesai
    part 11 tamat

--- page/detail_pengembalian.php
+++ page/detail_pengembalian.php
@@ -7,41 +7,79 @@
     <title>Detail Pengembalian</title>
 </head>
 <body>
+    <?php
+        $id_peminjaman = $_GET['id_peminjaman'];
+        if(empty($id_peminjaman)){
+            ?>
+                <script type="text/javascript">
+                    windows.location.href="?p=pengembalian";
+                </script>
+            <?php
+        }
+        $hari = date('d-m-y');
+        $d_peminjaman = "SELECT *, detail_pinjam.jumlah as jml FROM detail_pinjam left join peminjaman on peminjaman.id_peminjaman = detail_pinjam.id_peminjaman left join inventaris on inventaris.id_inventaris = detail_pinjam.id_inventaris left join pegawai on pegawai.id_pegawai = peminjaman.id_pegawai  WHERE peminjaman.id_peminjaman ='$id_peminjaman'" ;
+
+        $d_query = mysqli_query($koneksi,$d_peminjaman);
+        $data = mysqli_fetch_array($d_query);
+    ?>
     <div class="col-lg-6">
         <div class="panel panel-primary">
             <div class="panel-heading">Konfirmasi Pengembalian Inventaris</div>
             <div class="panel-body">
-                <form action="">
+                <form action="" method="post">
                     <div class="form-group">
                         <label for="" class="">Kode Peminjaman</label>
-                        <input type="text" name="" id="" class="form-control" readonly>
+                        <input type="text" name="id_peminjaman" id="" class="form-control" value="<?=$data['id_peminjaman']?>" readonly>
                     </div>
                     <div class="form-group">
                         <label for="" class="">Tanggal Peminjaman</label>
-                        <input type="date" name="" id="" class="form-control" readonly>
+                        <input type="text" name="tgl_pinjam" id="" class="form-control" value="<?=$hari?>" readonly>
                     </div>
                     <div class="form-group">
                         <label for="" class="">Nama Peminjam</label>
-                        <input type="text" name="" id="" class="form-control" readonly>
+                        <input type="text" name="nama_pegawai" id="" class="form-control" value="<?=$data['nama_pegawai']?>" readonly>
                     </div>
                     <div class="form-group">
                         <label for="" class="">Nama Barang</label>
-                        <input type="text" name="" id="" class="form-control" readonly>
+                        <input type="text" name="nama" id="" class="form-control" value="<?=$data['nama']?>" readonly>
                     </div>
                     <div class="form-group">
                         <label for="" class="">Jumlah</label>
-                        <input type="number" name="" id="" class="form-control" readonly>
+                        <input type="number" name="jml" id="" class="form-control" value="<?=$data['jml']?>" readonly>
                     </div>
                     <div class="form-group">
                         <label for="" class="">Tanggal Pengembalian</label>
-                        <input type="date" name="" id="" class="form-control">
+                        <input type="date" name="tgl_kembali" id="" class="form-control">
                     </div>
                     <div class="form-group">
-                        <button class="btn btn-primary btn-md" type="submit">Simpan</button>
+                        <button class="btn btn-primary btn-md" type="submit" name="simpan">Simpan</button>
                         <a href="?p=pengembalian" class="btn btn-md btn-default">Kembali</a>
                     </div>
                 </form>
             </div>
+            <?php
+                if(isset($_POST['simpan'])){
+                    $tgl_kembali = $_POST['tgl_kembali'];
+                    
+                    $sql_pengembalian = "UPDATE peminjaman SET tanggal_kembali = '$tgl_kembali', status_peminjaman = '2' WHERE id_peminjaman = '$id_peminjaman'";
+                    $q_pengembalian = mysqli_query($koneksi, $sql_pengembalian);
+
+                    if($q_pengembalian){
+                        ?>
+                            <script type="text/javascript">
+                                window.location.href="?p=pengembalian";
+
+                            </script>
+                        <?php
+                    }else{
+                        ?>
+                            <div class="alert alert-danger">
+                                Barang Gagal Untuk Diupdate!
+                            </div>
+                        <?php
+                    }
+                }
+            ?>
         </div>
     </div>
 </body>
--- page/pengembalian.php
+++ page/pengembalian.php
@@ -17,6 +17,7 @@
                     <thead>
                         <tr>
                             <th>No</th>
+                            <th>Kode peminjaman</th>
                             <th>Tanggal Pinjam</th>
                             <th>Nama Peminjam</th>
                             <th>Nama Barang</th>
@@ -27,7 +28,49 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
+                        <?php
+                            $hari = date('d,m,y');
+                            $d_peminjaman = "SELECT *, detail_pinjam.jumlah as jml FROM detail_pinjam left join peminjaman on peminjaman.id_peminjaman = detail_pinjam.id_peminjaman left join inventaris on inventaris.id_inventaris = detail_pinjam.id_inventaris left join pegawai on pegawai.id_pegawai = peminjaman.id_pegawai WHERE peminjaman.status_peminjaman ='1'" ;
+
+                            $d_query = mysqli_query($koneksi,$d_peminjaman);
+                            $cek = mysqli_num_rows($d_query);
+
+                            if($cek > 0){
+                                $no = 1;
+                                while($data_d = mysqli_fetch_array($d_query)){
+                                    ?>
+                                        <tr>
+                                            <td><?= $no++?></td>
+                                            <td><?= $data_d['id_peminjaman']?></td>
+                                            <td><?= $hari?></td>
+                                            <td><?= $data_d['nama_pegawai']?></td>
+                                            <td><?= $data_d['nama']?></td>
+                                            <td><?= $data_d['jml']?></td>
+                                            <td><?= $data_d['tanggal_kembali']?></td>
+                                            <td>
+                                                <?php
+                                                    if($data_d['status_peminjaman'] == '0'){
+                                                        echo"<label class='label label-danger'>Konfirmasi</label>";
+                                                    }else if($data_d['status_peminjaman'] == '1'){
+                                                        echo"<label class='label label-warning'>Dipinjam</label>";
+                                                    }else{
+                                                        echo"<label class='label label-success'>Dikembalikan</label>";
+                                                    }
+                                                ?>
+                                            </td>
+                                            <td>
+                                               <a href="?p=detail_pengembalian&id_peminjaman=<?=$data_d['id_peminjaman']?>" class="btn btn-sm btn-primary">Proses</a>
+                                            </td>
+                                        </tr>
+                                    <?php
+                                }
+                            }else{
+                                ?> 
+                                    <td colspan="9">Tidak ada data</td>
+                                <?php
+                            }
+                        ?>
+                        <!-- <tr>
                             <td>1</td>
                             <td>12-10-2023</td>
                             <td>Abdul Somat</td>
@@ -40,7 +83,7 @@
                             <td>
                                 <a href="?p=detail_pengembalian" class="btn btn-sm btn-primary ">Konfirmasi</a>
                             </td>
-                        </tr>
+                        </tr> -->
                     </tbody>
                 </table>
             </div>
